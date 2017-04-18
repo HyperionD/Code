af68a9b43663dd76cd9627ce2f592e1af6f2f7be
HyperionD <dongjj81@gmail.com>
2017-04-18 13:15:20 +0800

    finish timejs tabjs

--- web/tabjs/tab.js
+++ web/tabjs/tab.js
@@ -1,21 +1,31 @@
 /**
- * js实现标签页
- *
- * 使用示例 tab.html
- *
- * Tab(el) el为要实现标签页效果的元素，el元素下应该有classname为tab_title,tab_content的元素，
- * tab_title为标签页的标题，横向显示，默认第一项激活
- * tab_content为标签页的显示内容，顺序与tab_title相同，默认显示第一项内容
- * 激活的tab_title有id='active'
- * 显示的tab_content有id='show'
+ * Tab标签页效果
+ * @param {object} opt 参数选项
+ * class_name_for_tab:          tab元素的class name
+ * class_name_for_tab_title:    tab title元素的class name
+ * class_name_for_tab_content:  tab content元素的class name
  */
 
-function Tab(el) {
+function Tab(opt) {
+
+    // 默认参数
+    const default_opt = {
+        class_name_for_tab: 'tab',
+        class_name_for_tab_title: 'tab_title',
+        class_name_for_tab_content: 'tab_content'
+    };
+
+    //给定参数与默认参数结合,获取最终参数
+    opt = opt || default_opt;
+    const class_name_for_tab = opt.class_name_for_tab || default_opt.class_name_for_tab;
+    const class_name_for_tab_title = opt.class_name_for_tab_title || default_opt.class_name_for_tab_title;
+    const class_name_for_tab_content = opt.class_name_for_tab_conten || default_opt.class_name_for_tab_content;
 
     //获取元素
-    const tab_title = el.querySelector('.tab_title');
-    const tab_titles = el.querySelector('.tab_title').children;
-    const tab_contents = el.querySelector('.tab_content').children;
+    const tab = document.querySelector('.' + class_name_for_tab);
+    const tab_title = tab.querySelector('.' + class_name_for_tab_title);
+    const tab_titles = tab_title.children;
+    const tab_contents = tab.querySelector('.' + class_name_for_tab_content).children;
 
     // tab_title样式， 使用flex布局使tab_title横向排列
     tab_title.style.display = 'flex';
@@ -31,18 +41,18 @@ function Tab(el) {
     // 为了避免tab_contents少于tab_titles报错，所以需要验证tab_to_hide != null, tab_contents[i] != null
     for (let i=0; i<tab_titles.length; i++) {
         tab_titles[i].addEventListener('click', function () {
-            const tab_to_hide = el.querySelector('#show');
-            const tab_to_disactive = el.querySelector('#active');
+            const tab_to_hide = tab.querySelector('#show');
+            const tab_to_disactive = tab.querySelector('#active');
             tab_to_disactive.id = '';
             tab_titles[i].id = 'active';
-            if (tab_to_hide != null) {
+            if (tab_to_hide !== null) {
                 tab_to_hide.style.display = 'none';
                 tab_to_hide.id = '';
             }
-            if (tab_contents[i] != null) {
+            if (tab_contents[i] !== null) {
                 tab_contents[i].style.display = 'block';
                 tab_contents[i].id = 'show';
             }
-        }, false)
+        }, false);
     }
 }
